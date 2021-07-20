9aedade0343ba737934f3c89c240a5332521b02b
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-20 21:34:29 +0530

    [Ariprasath] added: Welcome note

---
+++ MainMethod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonUsingOOPS
+{
+    class MainMethod
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
